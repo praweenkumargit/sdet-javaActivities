d77cc43560d377791ba276d18ac952b90ab8c728
praweenkumargit <50977423+praweenkumargit@users.noreply.github.com>
2020-11-18 21:09:46 +0530

    Add files via upload

---
+++ Activity2_3.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package javaActivity2_3;
+
+/**
+ * @author PRAWEENKUMAR
+ *
+ */
+public class Activity2_3 {
+
+	
+	public static void main(String[] args) {
+		MountainBike mb = new MountainBike(3, 0, 25);
+        System.out.println(mb.bicycleDesc());
+        mb.speedUp(20);
+        mb.applyBrake(5);
+	}
+
+}
